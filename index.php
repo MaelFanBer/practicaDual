bedc2e7f7984af06ebcda72a386b0050b4093805
ManuelFandango <maelofcharity@gmail.com>
2023-11-30 13:08:55 +0100

    Relleno de contenido

--- index.php
+++ index.php
@@ -8,10 +8,23 @@
 <body>
     <h1>Random Number Generator (RNG)</h1>
     <p>
-        Número aleatorio: 
         <?php
-            echo random_int(1, 100);
+            if(isset($_GET["min"]) && isset($_GET["max"])) {
+                if ($_GET["max"] < $_GET["min"]) {
+                    echo "<span class='error'>*ERROR: El valor mínimo es más grande que el máximo. Por favor, escribelo de nuevo.<span>";
+                } else {
+                    echo "Numero aleatorio entre ".$_GET["min"]." y ".$_GET["max"].": ".random_int($_GET["min"], $_GET["max"]);
+                }
+            } else {
+                echo "Escribe un rango de números a usar: ";
+            }
         ?>
     </p>
+    <form action="index.php" method="get">
+        <h3>Elegir rango de números:</h3>
+        <input type="number" name="min" id="min" placeholder="valor mínimo">
+        <input type="number" name="max" id="max" placeholder="valor máximo">
+        <input type="submit" value="Asignar">
+    </form>
 </body>
 </html>
